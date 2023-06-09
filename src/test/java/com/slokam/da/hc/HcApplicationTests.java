ecb10a6b7aa74eb326895dc103d6512973b3fb56
Upendra Kristipati <upendrakristipati@Upendras-MacBook-Pro.local>
2023-06-09 16:20:44 +0530

    Initial Commit

---
+++ src/test/java/com/slokam/da/hc/HcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slokam.da.hc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
